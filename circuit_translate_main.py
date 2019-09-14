f48b542b348df2bbdd9704ac23e9d56b7b642d57
Hongxiang Chen <h.chen.17@ucl.ac.uk>
2019-09-14 10:23:21 +0200

    Drafted backward conversion

--- circuit_translate_main.py
+++ circuit_translate_main.py
@@ -1,10 +1,20 @@
-from collections import OrderedDict
+from collections import OrderedDict, namedtuple
+from typing import Dict
 
+import pyzx
 from pyzx.circuit import Circuit
-from qiskit.circuit import ClassicalRegister, QuantumCircuit, QuantumRegister
+from qiskit.circuit import ClassicalRegister, QuantumCircuit, QuantumRegister, \
+    Qubit
 from qiskit.dagcircuit import DAGCircuit
 
 from circuit_translator import check_classical_control, to_pyzx_gate
+
+Translated = namedtuple(
+    'Translated', ['circuit',
+                   'qreg_to_pyreg_range',
+                   'pyreg_range_to_qreg',
+                   'qregs', 'cregs'
+                   ])
 
 
 def dag_to_pyzx_circuit(dag: DAGCircuit):
@@ -18,7 +28,7 @@ def dag_to_pyzx_circuit(dag: DAGCircuit):
     """
     # In Qiskit, the quantum registers can have many diff names, whereas in PyZX circuit, there is only one global qubits.
     # We need a mapping from the global qubits back to the named qubits
-    pyreg_range_to_qreg = dict()  # map the start idx of the named qreg to the name
+    pyreg_range_to_qreg = dict()  # map the start idx of the named qreg to the qreg
     qreg_to_pyreg_range = dict()  # maps the qreg name to the start idx of global qreg
     tot_qb_count = 0
 
@@ -26,7 +36,7 @@ def dag_to_pyzx_circuit(dag: DAGCircuit):
     for qreg in dag.qregs.values():
         qreg_tmp = QuantumRegister(qreg.size, name=qreg.name)
         qregs[qreg.name] = qreg_tmp
-        pyreg_range_to_qreg[tot_qb_count] = qreg.name
+        pyreg_range_to_qreg[tot_qb_count] = qreg
         qreg_to_pyreg_range[qreg.name] = tot_qb_count
         tot_qb_count += qreg.size
 
@@ -35,7 +45,6 @@ def dag_to_pyzx_circuit(dag: DAGCircuit):
         creg_tmp = ClassicalRegister(creg.size, name=creg.name)
         cregs[creg.name] = creg_tmp
 
-    name = dag.name or None
     gates = []
 
     for node in dag.topological_op_nodes():
@@ -60,6 +69,48 @@ def dag_to_pyzx_circuit(dag: DAGCircuit):
             to_pyzx_gate(inst, qubits, gates,
                          clbits=clbits, condition=node.condition)
 
-    circuit = Circuit(tot_qb_count, name='')
+    name = '' if dag.name is None else str(dag.name)
+    circuit = Circuit(tot_qb_count, name=name)
     circuit.gates = gates
-    return circuit, qreg_to_pyreg_range, pyreg_range_to_qreg
+    return Translated(
+        circuit, qreg_to_pyreg_range, pyreg_range_to_qreg,
+        list(qregs.values()), list(cregs.values()))
+
+
+def add_non_unitary_gate(
+        gate: pyzx.gates.Gate,
+        pyreg_to_qubit: Dict[int, Qubit],
+        dagcircuit: DAGCircuit):
+    if not isinstance(gate, pyzx.gates.Nonunitary):
+        return False
+    dagcircuit.apply_operation_back(
+        op= # qiskit gate,
+        qargs=[pyreg_to_qubit[target] for target in gate.target],
+        cargs=gate.stored_data['clbits'],
+        condition=gate.stored_data['control'],
+    )
+    return True
+
+
+def pyzx_circ_to_dag(
+        circ: pyzx.circuit.Circuit, translated: Translated):
+    dagcircuit = DAGCircuit()
+    dagcircuit.name = circ.name
+    pyreg_range_to_qreg = translated.pyreg_range_to_qreg
+    # produce a lookup table from the pyreg_range_to_qreg
+    # TODO(hx) Maybe we should do this inside dag_to_pyzx_circuit
+    pyreg_to_qubit = dict()
+    for start_idx, qreg in pyreg_range_to_qreg.items():
+        for idx in range(start_idx, qreg.size):
+            pyreg_to_qubit[idx] = qreg[idx - start_idx]
+
+    [dagcircuit.add_qreg(qreg) for qreg in translated.qregs]
+    [dagcircuit.add_creg(creg) for creg in translated.cregs]
+
+    instructions = []
+    for gate in circ.gates:
+        if add_non_unitary_gate(
+                gate, pyreg_to_qubit, dagcircuit):
+            pass
+        else:
+            add_normal_gate(gate, pyreg_to_qubit, dagcircuit)
